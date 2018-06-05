7ee79802e6657c756b349635df73a885a48a4853
Sergey <verstka.sv@mail.rugit config --global user.email verstka.sv@mailgit config --global user.email verstka.sv@mail.ru>
2018-06-05 22:32:00 +0300

    shiza)

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var less = require('gulp-less');
+
+gulp.task('server', ['styles'], function() {
+    browserSync.init({
+    	server: { baseDir: './app/'}
+    });
+    gulp.watch('./app/**/*.html').on('change', browserSync.reload);
+    gulp.watch('./app/less/**/*.less', ['styles']);
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./app/less/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('default', ['server']);
